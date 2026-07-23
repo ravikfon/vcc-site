39b057f86e526250c3d3da5fdc4a81bbb8d49771
ravikfon <151067375+ravikfon@users.noreply.github.com>
2026-07-23 15:26:16 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,18 @@
+function entrar(){
+
+let usuario = document.getElementById("usuario").value;
+let senha = document.getElementById("senha").value;
+
+if(usuario === "admin" && senha === "1234"){
+
+    localStorage.setItem("adminLogado","true");
+
+    window.location="painel.html";
+
+}else{
+
+    document.getElementById("msg").innerHTML="Login incorreto";
+
+}
+
+}
